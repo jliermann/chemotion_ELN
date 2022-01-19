2a20d52aae1f040d1bb7863b8e28b2fe708aacfe
Johannes Liermann <liermann@uni-mainz.de>
2022-01-19 16:54:21 +0100

    fix envvar script

--- scripts/env_proxy_setup.sh
+++ scripts/env_proxy_setup.sh
@@ -4,7 +4,7 @@
 
 keywords=("SSH" "http" "HTTP")
 loops=${#keywords[@]}
-filename="/home/production/jgu_env.txt"
+filename="/home/production/jgu_env.sh"
 
 # Determine number of env for each keyword
 
@@ -20,8 +20,11 @@ for (( i=0; i<$loops; i++ )); do
     for (( j=0; j<${length[i]}; j++)); do
         envvar=$(env | grep ${keywords[i]} | sed -n "$(expr $j + 1) p")
         echo "EXPORT $envvar" >> $filename
+        echo $envvar
     done
     # echo ${keywords[i]}
     # length[i]=$(env | grep ${keywords[i]} | wc -l)
     # echo ${length[i]}
 done
+
+chmod +x $filename
