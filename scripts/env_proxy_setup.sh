e6bb482731f03fea95731f8780d43ea2f2fa3801
Johannes Liermann <liermann@uni-mainz.de>
2022-01-19 16:40:48 +0100

    fix loop

--- scripts/env_proxy_setup.sh
+++ scripts/env_proxy_setup.sh
@@ -16,8 +16,8 @@ done
 # Assemble export statements
 
 for (( i=0; i<$loops; i++ )); do
-    for (( j=1; j=${length[i]}; j++)); do
-        envvar=$(env | grep ${keywords[i]} | sed -n "$j p")
+    for (( j=0; j<${length[i]}; j++)); do
+        envvar=$(env | grep ${keywords[i]} | sed -n "$(j+1) p")
         echo "EXPORT $envvar"
     done
     # echo ${keywords[i]}
