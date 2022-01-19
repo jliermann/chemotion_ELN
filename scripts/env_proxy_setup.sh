4aeca58d6a8c0d49261b1dc4b61d4e5047c8cab5
Johannes Liermann <liermann@uni-mainz.de>
2022-01-19 16:31:34 +0100

    work on env

--- scripts/env_proxy_setup.sh
+++ scripts/env_proxy_setup.sh
@@ -5,9 +5,22 @@
 keywords=("SSH" "http" "HTTP")
 loops=${#keywords[@]}
 
+# Determine number of env for each keyword
+
 for (( i=0; i<$loops; i++ )); do
     echo ${keywords[i]}
     length[i]=$(env | grep ${keywords[i]} | wc -l)
     echo ${length[i]}
 done
 
+# Assemble export statements
+
+for (( i=0; i<$loops; i++ )); do
+    for (( j=0; j<${lenght[i]}; j++)); do
+        envvar=$(env | grep $keywords[i] | sed -n '$j{p;q}')
+        echo "EXPORT $envvar"
+    done
+    # echo ${keywords[i]}
+    # length[i]=$(env | grep ${keywords[i]} | wc -l)
+    # echo ${length[i]}
+done
