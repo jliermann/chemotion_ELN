ecacb89aa9fd50f446a441d7031b358e9553357f
Johannes Liermann <liermann@uni-mainz.de>
2022-01-20 15:25:37 +0100

    improve env matching

--- scripts/env_proxy_setup.sh
+++ scripts/env_proxy_setup.sh
@@ -2,29 +2,21 @@
 
 # Script for retrieving all environment variables concerning webproxy and SSH and passing them to the deploy script
 
+# Define Variables
+
 keywords=("SSH" "http" "HTTP")
 loops=${#keywords[@]}
 filename="/home/production/jgu_env.sh"
 
-# Determine number of env for each keyword
-
-for (( i=0; i<$loops; i++ )); do
-    # echo ${keywords[i]}
-    length[i]=$(env | grep ${keywords[i]} | wc -l)
-    # echo ${length[i]}
-done
-
 # Assemble export statements
 
 for (( i=0; i<$loops; i++ )); do
-    for (( j=0; j<${length[i]}; j++)); do
-        envvar=$(env | grep ${keywords[i]} | sed -n "$(expr $j + 1) p")
-        echo "EXPORT $envvar" >> $filename
-        echo $envvar
+    length=$(env | grep ${keywords[i]} | wc -l)  # Determine number of env for each keyword
+    for (( j=0; j<$length; j++)); do
+        envname=$(env | grep ${keywords[i]} | sed -n "$(expr $j + 1) p" | sed  's|(\w+)=|\2 \1|')
+#        echo "EXPORT $envvar" >> $filename
+        echo $envname
     done
-    # echo ${keywords[i]}
-    # length[i]=$(env | grep ${keywords[i]} | wc -l)
-    # echo ${length[i]}
 done
 
 chmod +x $filename
