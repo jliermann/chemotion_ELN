34d9918728dfedd1cf1517418d099276aa6d332f
Johannes Liermann <liermann@uni-mainz.de>
2022-10-11 10:19:46 +0200

    chore: output check

--- scripts/delayed_jobs_watchdog.sh
+++ scripts/delayed_jobs_watchdog.sh
@@ -4,6 +4,13 @@ prodDir="/var/www/chemotion_ELN/current"
 
 cd $prodDir
 
-delayedOutput=$("source ~/.profile && RAILS_ENV=production bundle exec bin/delayed_job status")
+delayedOutput=$(source ~/.profile && RAILS_ENV=production bundle exec bin/delayed_job status)
+match="no instances running"
 
 echo $delayedOutput
+
+if [[ "$delayedOutput" == *"$match"* ]]; then
+    echo "Delayed processes stopped."
+else
+    echo "Delayed jobs running."
+fi
