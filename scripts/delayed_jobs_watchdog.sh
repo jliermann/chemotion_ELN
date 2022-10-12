12e06c8118904ef31256680cfd60be1a1e449ca7
Johannes Liermann <liermann@uni-mainz.de>
2022-10-12 17:46:19 +0200

    chore: improved mails, added logging

--- scripts/delayed_jobs_watchdog.sh
+++ scripts/delayed_jobs_watchdog.sh
@@ -7,6 +7,13 @@ restart="source ~/.profile && RAILS_ENV=production bundle exec bin/delayed_job r
 mail="liermann@uni-mainz.de"
 success="Restart of Chemotion delayed jobs on $(hostname) successful"
 fail="Restart of Chemotion delayed jobs on $(hostname) unsuccesful"
+log="/var/log/delayed_jobs_watchdog"
+
+failfunction() {
+    echo "$(date): $fail" | mail -s "$fail" $mail 
+    echo "$(date): $fail" > $log
+    exit 1
+}
 
 cd $prodDir
 
@@ -17,9 +24,10 @@ match="no instances running"
 # echo $delayedOutput
 
 if [[ "$delayedOutput" == *"$match"* ]]; then
-    echo "Delayed processes stopped. Restarting now..."
-    eval $restart || echo "$(date): $fail" | mail -s $fail $mail && exit 1
-    echo "$(date): $success" | mail -s $success $mail
+    echo "Delayed processes stopped. Restarting now..." | echo "$(date): Restarting delayed jobs." >> $log
+    eval $restart || failfunction()
+    echo "$(date): $success" | mail -s "$success" $mail 
+    echo "$(date): $success" > $log
 else
     echo "Delayed jobs running."
 fi
