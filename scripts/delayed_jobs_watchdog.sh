8428c7bf19c8e2deca6783b7d0606443b3645020
Johannes Liermann <liermann@uni-mainz.de>
2022-10-12 17:29:43 +0200

    chore: add action and mail notif.

--- scripts/delayed_jobs_watchdog.sh
+++ scripts/delayed_jobs_watchdog.sh
@@ -2,17 +2,22 @@
 
 prodDir="/var/www/chemotion_ELN/current"
 tmp="/tmp/delayed_out"
+check="source ~/.profile && RAILS_ENV=production bundle exec bin/delayed_job status"
+restart="source ~/.profile && RAILS_ENV=production bundle exec bin/delayed_job restart"
+mail="liermann@uni-mainz.de"
 
 cd $prodDir
 
-source ~/.profile && RAILS_ENV=production bundle exec bin/delayed_job status 2> $tmp
+eval $check 2> $tmp
 delayedOutput=$( cat $tmp )
 match="no instances running"
 
 # echo $delayedOutput
 
 if [[ "$delayedOutput" == *"$match"* ]]; then
-    echo "Delayed processes stopped."
+    echo "Delayed processes stopped. Restarting now..."
+    eval $restart || mail -s "Restart of Chemotion delayed jobs on $(hostname) unsuccesful" $mail && exit 1
+    mail -s "Restart of Chemotion delayed jobs on $(hostname) succesful" $mail
 else
     echo "Delayed jobs running."
 fi
