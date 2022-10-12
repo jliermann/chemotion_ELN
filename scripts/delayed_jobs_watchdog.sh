7fbdf7b1010c5bc1a9c5eedb2e80aa1c39f38e35
Johannes Liermann <liermann@uni-mainz.de>
2022-10-12 17:22:57 +0200

    fix: output mapping

--- scripts/delayed_jobs_watchdog.sh
+++ scripts/delayed_jobs_watchdog.sh
@@ -5,10 +5,11 @@ tmp="/tmp/delayed_out"
 
 cd $prodDir
 
-delayedOutput=$( source ~/.profile && RAILS_ENV=production bundle exec bin/delayed_job status | tee $tmp )
+source ~/.profile && RAILS_ENV=production bundle exec bin/delayed_job status 2> $tmp
+delayedOutput=$( cat $tmp )
 match="no instances running"
 
-echo $delayedOutput
+# echo $delayedOutput
 
 if [[ "$delayedOutput" == *"$match"* ]]; then
     echo "Delayed processes stopped."
