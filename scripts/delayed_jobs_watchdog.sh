e79474312f6302d6201023a156f03e49ade4742c
Johannes Liermann <liermann@uni-mainz.de>
2022-10-12 17:54:15 +0200

    fix: minor issues

--- scripts/delayed_jobs_watchdog.sh
+++ scripts/delayed_jobs_watchdog.sh
@@ -4,14 +4,11 @@ prodDir="/var/www/chemotion_ELN/current"
 tmp="/tmp/delayed_out"
 check="source ~/.profile && RAILS_ENV=production bundle exec bin/delayed_job status"
 restart="source ~/.profile && RAILS_ENV=production bundle exec bin/delayed_job restart"
-mail="liermann@uni-mainz.de"
-success="Restart of Chemotion delayed jobs on $(hostname) successful"
-fail="Restart of Chemotion delayed jobs on $(hostname) unsuccesful"
 log="/var/log/delayed_jobs_watchdog"
 
-failfunction() {
+function failfunction{
     echo "$(date): $fail" | mail -s "$fail" $mail 
-    echo "$(date): $fail" > $log
+    echo "$(date): $fail" >> $log
     exit 1
 }
 
@@ -24,10 +21,10 @@ match="no instances running"
 # echo $delayedOutput
 
 if [[ "$delayedOutput" == *"$match"* ]]; then
-    echo "$(date): Delayed processes stopped. Restarting now..." | tee $log
-    eval $restart || failfunction()
+    echo "$(date): Delayed processes stopped. Restarting now..." | tee -a $log
+    eval $restart || failfunction
     echo "$(date): $success" | mail -s "$success" $mail 
-    echo "$(date): $success" > $log
+    echo "$(date): $success" >> $log
 else
-    echo "$(date): Delayed jobs running." | tee $tmp
+    echo "$(date): Delayed jobs running." | tee -a $log
 fi
