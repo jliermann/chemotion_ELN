15f086a53217aa642a63da9bd2c8564b30c82928
Johannes Liermann <liermann@uni-mainz.de>
2022-10-12 16:59:50 +0200

    fix: string match

--- scripts/delayed_jobs_watchdog.sh
+++ scripts/delayed_jobs_watchdog.sh
@@ -4,12 +4,13 @@ prodDir="/var/www/chemotion_ELN/current"
 
 cd $prodDir
 
-delayedOutput=$(source ~/.profile && RAILS_ENV=production bundle exec bin/delayed_job status)
+source ~/.profile && RAILS_ENV=production bundle exec bin/delayed_job status > /tmp/delay.out
+delayedOutput=$(cat "/tmp/delay.out")
 match="no instances running"
 
 echo $delayedOutput
 
-if [[ echo "$delayedOutput" | grep "$match" ]]; then
+if [[ "$delayedOutput" == *"$match"* ]]; then
     echo "Delayed processes stopped."
 else
     echo "Delayed jobs running."
