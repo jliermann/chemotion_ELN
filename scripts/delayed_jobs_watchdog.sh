6d7f38128fa72bc355e09f14fa6e5f8a2a7494d6
Johannes Liermann <liermann@uni-mainz.de>
2022-10-12 18:22:13 +0200

    chore: add comments to code

--- scripts/delayed_jobs_watchdog.sh
+++ scripts/delayed_jobs_watchdog.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+
+# Variables
+
+mail="liermann@uni-mainz.de" # Add your mail here
+
 prodDir="/var/www/chemotion_ELN/current"
 tmp="/tmp/delayed_out"
 check="source ~/.profile && RAILS_ENV=production bundle exec bin/delayed_job status"
 restart="source ~/.profile && RAILS_ENV=production bundle exec bin/delayed_job restart"
-mail="liermann@uni-mainz.de"
 success="Restart of Chemotion delayed jobs on $(hostname) successful"
 fail="Restart of Chemotion delayed jobs on $(hostname) unsuccesful"
 log="/var/log/delayed_jobs_watchdog"
+
+match="no instances running"  # Output criterium for non-running jobs
+
+
+# Notification in case of failure
 
 failfunction() {
     echo "$(date): $fail" | mail -s "$fail" $mail 
@@ -15,19 +24,25 @@ failfunction() {
     exit 1
 }
 
+
+# Working directory
+
 cd $prodDir
+
+
+# Execute status and write output to variable
 
 eval $check 2> $tmp
 delayedOutput=$( cat $tmp )
-match="no instances running"
+
 
 # echo $delayedOutput
 
-if [[ "$delayedOutput" == *"$match"* ]]; then
+if [[ "$delayedOutput" == *"$match"* ]]; then # ... then jobs are not running
     echo "$(date): Delayed processes stopped. Restarting now..." | tee -a $log
     eval $restart || failfunction
     echo "$(date): $success" | mail -s "$success" $mail 
     echo "$(date): $success" >> $log
-else
+else # ... everything seems normal
     echo "$(date): Delayed jobs running." | tee -a $log
 fi
