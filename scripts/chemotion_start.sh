12f40b6fafdbdee12e87a677199a11653950f43e
Johannes Liermann <liermann@uni-mainz.de>
2022-11-28 14:31:35 +0100

    chore: chemotion start more verbose

--- scripts/chemotion_start.sh
+++ scripts/chemotion_start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 sleep 60
+
+echo "$(date): Starting Chemotion docker instance..."
 cd /home/production/chemotion
-docker compose up -d
+if docker compose up -d ; then
+    echo "$(date): Chemotion started successfully."
+else
+    echo "$(date): Chemotion start unsuccessful."
+fi
